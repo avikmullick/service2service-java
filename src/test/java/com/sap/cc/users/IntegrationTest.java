5c671cf767ab1d0e966507af996c44c496277a94
Marco Spieß <marco.spiess02@sap.com>
2021-07-22 16:43:38 +0200

    add integration test

---
+++ src/test/java/com/sap/cc/users/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.sap.cc.users;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void returnsPrettyPage() {
+        String prettyPage = restTemplate.getForEntity("/api/v1/users/pretty/1", String.class).getBody();
+        assertThat(prettyPage).contains("\n");
+    }
+}
