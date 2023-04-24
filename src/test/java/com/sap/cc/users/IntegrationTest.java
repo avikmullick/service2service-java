872eab4624cf5cff713922e5f46f0993d2ba1bd8
Mark De Vries <mar.de.vries@sap.com>
2023-04-24 15:04:36 +0200

    S2s wip (#90)
    * Update IntegrationTest.java
    
    * Update application.properties
    
    * Update application.properties
    
    * Update IntegrationTest.java

--- src/test/java/com/sap/cc/users/IntegrationTest.java
+++ src/test/java/com/sap/cc/users/IntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,7 +15,10 @@ public class IntegrationTest {
 
     @Autowired
     private WebTestClient webTestClient;
-
+    
+    @Autowired
+    private ObjectMapper objectMapper; 
+    
     @Test
     @Disabled
     void returnsPrettyPage() {
@@ -24,8 +27,8 @@ public class IntegrationTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).value(response -> {
-                    assertThat(response).contains("\r\n");
+                .expectBody(String.class).value(body -> {
+                    assertThat(body).contains("\r\n");
                 });
     }
 }
